4deb1211d911db80300440a903983c9c2cc971cb
Sebastian Wehrmann <sebastian@wehrmann.de>
2014-09-20 15:34:14 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
 
 setup(
     name='gocept.recipe.bowerstaticbundler',
-    version='1.0.0',
+    version='1.0.1.dev0',
     url='https://vcs.verdi4you.de/gocept.recipe.bowerstaticbundler',
     license='GPL',
     description='Minifies and bundles JS and CSS files included with '
